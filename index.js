ee84121d3d6dc84808998fa9ac3fc66e3ad5f9ba
ZakAttakk <zjbrewer@gmail.com>
2022-10-05 19:00:01 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// 0. Make an empty div on the HTML page.  Give it the Bootstrap class "container-md" 
+
+// 1. Put a Bootstrap row and three Bootstrap columns in your container div.  Reminder of how to do it here: getbootstrap.com/docs/5.2/layout/grid/
+
+// 2. In the first column, make a text input box on the HTML page.  Style it with a Bootstrap class.  Help here: getbootstrap.com/docs/5.2/forms/form-control/
+
+// 3. In the second column, make a button on the HTML page.  Style it with a Bootstrap class.  Underneath the button make a div with an ID called "things"
+
+// 4. In the JavaScript file, add a "click" event listener to the button.  Make the button log "it works!" when you click it.  Next, see if you can get it to log the value of whatever is typed in the the input box.  You might have to Google "get value from input javascript".
+
+// 5. You can add event listeners to the window object just like you can add one to a button.  Google how to add a "resize" event listener to the window, and then do it.  Make it so that every time you resize the window, something is logged to the console.
+
+// 6. Now Google "get window width javascript".  Alter what you did in number 4 so that your window's width is printed to the console everytime you resize your window.
+
+// 7. Use if-statements (conditionals) to make it log  "skinny" if the window width is less than or equal to 768, make it log "wide" for any window larger than 768
+
+// 8. Return to what you worked on for question 4.  Make it so that when you click the button it it creates a new element <p> element, set the innerHTML of that element to the value of the imput, and appends the new element to the div with ID "things"
+
+// 9. Download an image and put it in root folder of this project.  Add the image to the third column of your HTML file.  Give it Bootstrap's responsive image class.
+
+//10.  Google how to add/remove classes from an HTML element.  Also, Google how to hide an image using either a Bootstrap class or traditionl CSS.  In your what you made for #7, make it so that the image hides if the window width is <= 768
